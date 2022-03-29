5179bfe9b2a52f1fc5cf7ae8c2844c4cbb92c246
Adnan Shaikh <aa2232783@gmail.com>
2022-03-29 12:42:32 +0530

    Shubham

--- stduid/core/urls.py
+++ stduid/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import homepage, stdid, collegeid
+from .views import homepage, stdid, collegeid, studentDataRetrivel
 
 urlpatterns = [
     path("",homepage, name="homepage" ),
     path("stdid",stdid, name="stdid"),
     path("clgid",collegeid, name= "clgid"),
-    
+    path("studentData", studentDataRetrivel, name="studentdata")
 ]
--- stduid/core/views.py
+++ stduid/core/views.py
@@ -1,3 +1,5 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,4 +12,19 @@ def stdid(req):
 def collegeid(req):
     return render(req,"clgid.html")
 
+def  studentDataRetrivel(req):
+    
+    stateId = req.POST.get("stateId")
+    uniId = req.POST.get("uniId")
+    collegeId = req.POST.get("collegeId")
+    grNo = req.POST.get("grNo")
+    
+    context = {
+        "state": stateId,
+        "uniId": uniId,
+        "collegeId": collegeId,
+        "grNo": grNo,
+    }
+    
+    return JsonResponse(json.dumps(context), safe=False)
 
